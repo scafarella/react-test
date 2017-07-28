130b60ac4bb25ef03b0fc49bc7c9ba385a6113c3
stefano.cafarella <s.cafarella@reply.it>
2017-07-28 17:10:48 +0200

    first commit: hello world

---
+++ index.js
@@ -0,0 +1,12 @@
+const express = require('express')
+const app = express()
+
+app.set('view engine', 'pug')
+
+app.get('/', function (req, res) {
+   res.render('index', { title: 'Hey', message: 'Hello there!' })
+})
+
+app.listen(3000, function () {
+  console.log('Listening on port 3000!')
+})

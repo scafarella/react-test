a14a6363a4b069492189591a01ae320ca1365395
stefano.cafarella <s.cafarella@reply.it>
2017-08-04 11:53:28 +0200

    refactor apis

---
+++ server/apis/product.js
@@ -0,0 +1,15 @@
+const HttpProxyAgent = require('http-proxy-agent');
+
+const product = {
+  all(){
+    return fetch('http://www.matchesfashion.com/nms/ajax/p/1095470,1095483,1095472,1095467',{
+      agent: new HttpProxyAgent({
+        host: 'proxy.reply.it',
+        port: '8080'
+      })
+    })
+    .then(response => response.json())
+  }
+}
+
+module.exports = product
--- server/controllers/product.controller.js
+++ server/controllers/product.controller.js
@@ -1,15 +1,9 @@
-const HttpProxyAgent = require('http-proxy-agent');
+const product = require('../apis/product')
 
 const productController = {
 
   all(req,res){
-      fetch('http://www.matchesfashion.com/nms/ajax/p/1095470,1095483,1095472,1095467',{
-        agent: new HttpProxyAgent({
-          host: 'proxy.reply.it',
-          port: '8080'
-        })
-      })
-      .then(response => response.json())
+      product.all()
       .then(data => res.json(data))
       .catch(err => {
         res.json({code: 'GENERIC_ERROR', error:'Generic Error'})
